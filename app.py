26464f923a5725b7e3d036803c61a55269094494
jauumgod <69704112+jauumgod@users.noreply.github.com>
2023-02-06 11:02:55 -0300

    add more functions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, request, render_template, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_manager, login_user, logout_user
+from flask_login import LoginManager, login_manager, login_user, logout_user,UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -8,11 +8,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ford.db"
 app.config["SQLALCHEMY_TRACKMODIFICATIONS"] = False
 app.config['SECRET_KEY']='uzumymw' #trpaça do gta kkkk
 db = SQLAlchemy()
-login_manager = LoginManager()
+login_manager = LoginManager(app)
 db.init_app(app)
 
 
-class User(db.Model):
+@login_manager.user_loader
+def current_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(184), unique=True, nullable=False)
@@ -23,6 +27,7 @@ class User(db.Model):
 
     with app.app_context():
         db.create_all()
+    
 
 class Devedores(db.Model):
     __tablename__ = 'tab_clients'
@@ -38,26 +43,25 @@ class Devedores(db.Model):
         db.create_all()
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
+@app.route("/config/user/<int:id>")
+def unique(id):
+    user = User.query.get(id)
+    return render_template("config", user=user)
 
+@app.route("/config/delete/<int:id>")
+def deleteUser(id):
+    user = User.query.filter_by(id=id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("config"))
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    
     if request.method=='POST':
         username = request.form['usuario']
         password = request.form['senha']
         
-        user = User.query.filter_by(login=username).first()
-        if not user:
-            flash('Credenciais Inválidas')
-            return redirect(url_for("login"))
-
-        if not check_password_hash(user.password, password):
-            flash('Credenciais Inválidas')
-            return redirect(url_for("login"))
-
         #login_user(user)
         return redirect(url_for("index"))
 
@@ -68,15 +72,6 @@ def route_admin_secret():
     if request.method=='POST':
         username = request.form['usuario']
         password = request.form['senha']
-        
-        user = User.query.filter_by(login=username).first()
-        if not user:
-            flash('Credenciais Inválidas')
-            return redirect(url_for("login"))
-
-        if not password(user.password, password):
-            flash('Credenciais Inválidas')
-            return redirect(url_for("login"))
             
         #login_user(user)
         return redirect(url_for("index"))
@@ -85,15 +80,24 @@ def route_admin_secret():
 def index():
     return render_template("index.html")
 
-@app.route("/settings")
-def settings():
+@app.route("/config")
+def config():
+    
+    users = User.query.all()
+    return render_template("config.html", users=users)
 
-    return render_template("settings.html")
 
-@app.route("/settings/registrar")
+@app.route("/registrar", methods=['GET', 'POST'])
 def registrar():
+    if request.method=='POST':
+        user = User()
+        user.username = request.form['user']
+        user.password = request.form['password']
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("config"))
 
-    return render_template("register_user.html")
+    return render_template("registrar.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- venv-appclient/Scripts/activate.bat
+++ venv-appclient/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\joao.marinho\Desktop\PYTHON\client_manager\appclient-manager\venv-appclient
+set VIRTUAL_ENV=C:\Users\joao.marinho\Desktop\Projeto\PYTHON\proj\appclient-manager\venv-appclient
 
 if not defined PROMPT set PROMPT=$P$G
 
